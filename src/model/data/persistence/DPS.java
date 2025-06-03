3f8018ec8ec159850292bcdcfced26f5141c3a60
Bastian <bastian.leouedec@gmail.com>
2025-06-03 22:03:26 +0200

    Classes DPS, Journée et Sport

--- src/model/data/persistence/DPS.java
+++ src/model/data/persistence/DPS.java
@@ -1,5 +1,9 @@
 package model.data.persistence;
 
 public class DPS {
-    
+    private long id;
+    private int[] horaire_depart;
+    private int[] horaire_fin;
+    private Journée programme;
+    // Reste à faire
 }
--- src/model/data/persistence/Sport.java
+++ src/model/data/persistence/Sport.java
@@ -1,5 +1,37 @@
 package model.data.persistence;
 
 public class Sport {
-    
+    private String code;
+    private String nom;
+
+    public Sport(String code, String nom){
+        if (code == null || code.trim().equals("") || nom == null || nom.trim().equals("")){
+            throw new IllegalArgumentException("Paramètres invalides");
+        }
+
+        this.code = code;
+        this.nom = nom;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public void setCode(String newCode){
+        if (newCode == null || newCode.trim().equals("")){
+            throw new IllegalArgumentException("Paramètre invalide");
+        }
+        this.code = newCode;
+    }
+
+    public void setNom(String newNom){
+        if(newNom == null || newNom.trim().equals("")){
+            throw new IllegalArgumentException("Paramètre invalide");
+        }
+        this.nom = newNom;
+    }
 }
