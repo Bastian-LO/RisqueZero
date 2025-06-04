84c37f93ff8b863bdc09f6762826f19af301ca14
Bastian <bastian.leouedec@gmail.com>
2025-06-04 09:17:01 +0200

    javaDoc et code fini de Site.java

--- src/model/data/persistence/Site.java
+++ src/model/data/persistence/Site.java
@@ -1,11 +1,38 @@
 package model.data.persistence;
 
+/**
+ * Class initializing a place where an event will take place
+ */
 public class Site {
+
+    /**
+     * The code of the place
+     */
     private String code;
+
+    /**
+     * The name of the place
+     */
     private String nom;
+
+    /**
+     * The longitude coordinate of the place
+     */
     private float longitude;
+
+    /**
+     * The latitude coordinate of the place
+     */
     private float latitude;
 
+    /**
+     * Constructor
+     * @param code The code of the place
+     * @param nom The name of the place
+     * @param longitude The longitude coordinate of the place
+     * @param latitude The latitude coordinate of the place
+     * @throws IllegalArgumentException thrown if the parameters are incorrect
+     */
     public Site(String code, String nom, float longitude, float latitude) throws IllegalArgumentException{
         if(code == null || code.trim().equals("") || nom == null || nom.trim().equals("") || 
         longitude < -180 || longitude > 180 || latitude < -90 || latitude > 90){
@@ -18,22 +45,42 @@ public class Site {
         this.latitude = latitude;
     }
 
+    /**
+     * Getter for the code
+     * @return the code
+     */
     public String getCode(){
         return this.code;
     }
 
+    /**
+     * Getter for the name
+     * @return the name
+     */
     public String getNom(){
         return this.nom;
     }
 
+    /**
+     * Getter for the longitude
+     * @return the longitude
+     */
     public float getLongitude(){
         return this.longitude;
     }
 
+    /**
+     * Getter for the latitude
+     * @return the latitude
+     */
     public float getLatitude(){
         return this.latitude;
     }
 
+    /**
+     * Setter for the code
+     * @param newCode the new code
+     */
     public void setCode(String newCode){
         if (newCode == null || newCode.trim().equals("")){
             throw new IllegalArgumentException("setCode : paramètre invalide");
@@ -42,11 +89,39 @@ public class Site {
         this.code = newCode;
     }
 
+    /**
+     * Setter for the name
+     * @param newNom the new name
+     */
     public void setNom(String newNom){
         if (newNom == null || newNom.trim().equals("")){
             throw new IllegalArgumentException("setNom : paramètre invalide");
         }
 
         this.nom = newNom;
+    }
+
+    /**
+     * Setter for the longitude
+     * @param newLongitude the new longitude
+     */
+    public void setLongitude(float newLongitude){
+        if (newLongitude < -180 || newLongitude > 180){
+            throw new IllegalArgumentException("setLongitude : paramètre invalide");
+        }
+
+        this.longitude = newLongitude;
+    }
+
+    /**
+     * Setter for the latitude
+     * @param newLatitude the new latitude
+     */
+    public void setLatitude(float newLatitude){
+        if (newLatitude < -90 || newLatitude > 90){
+            throw new IllegalArgumentException("setLatitude : paramètre invalide");
+        }
+
+        this.latitude = newLatitude;
     }
 }
