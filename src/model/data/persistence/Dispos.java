58c286a3c42e622c53cfd852d1b5cdc62414adc8
Enrick <mananjean.enrick@gmail.com>
2025-06-04 09:21:53 +0200

    Création du constructeur Dispos

--- src/model/data/persistence/Dispos.java
+++ src/model/data/persistence/Dispos.java
@@ -1,11 +1,11 @@
 package model.data.persistence;
 
 public class Dispos {
-    private Journée date;
+    private Journee date;
     private int[] heureDebut;
     private int[] heureFin;
 
-    public Dispos(Journée date, int[] heureDebut, int[] heureFin) throws IllegalArgumentException {
+    public Dispos(Journee date, int[] heureDebut, int[] heureFin) throws IllegalArgumentException {
         // Checks if the parameters are valid
         if (date == null || heureDebut == null || heureFin == null || heureDebut[0] < 0 || heureDebut[1] < 0 || heureFin[0] < 0 || heureFin[1] < 0 || heureDebut[0] > 23 || heureDebut[1] > 59 || heureFin[0] > 23 || heureFin[1] > 59 ) {
             throw new IllegalArgumentException("Les paramètres ne peuvent pas être null ou vides");
