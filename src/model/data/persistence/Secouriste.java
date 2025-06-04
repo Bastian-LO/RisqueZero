810a8e8768d1315c06851c6db71deea7190dde21
Bastian <bastian.leouedec@gmail.com>
2025-06-04 09:30:36 +0200

    Changements mineurs dans Secouriste.java

--- src/model/data/persistence/Secouriste.java
+++ src/model/data/persistence/Secouriste.java
@@ -9,14 +9,14 @@ public class Secouriste{
     private String email;
     private String tel;
     private String adresse;
-    private HashSet<Dispos> disponibilité;
+    private HashSet<Dispos> disponibilite;
 
-    public Secouriste(long id, String nom, String prenom, String dateNaissance, String email, String tel, String adresse) throws IllegalArgumentException{
+    public Secouriste(long id, String nom, String prenom, String dateNaissance, String email, String tel, String adresse, HashSet<Dispos> disponibilite) throws IllegalArgumentException{
         // Checks if the parameters are valid
         if(id < 1 || nom == null || nom.trim().equals("") || nom == null || nom.trim().equals("") || 
         prenom == null || prenom.trim().equals("") || dateNaissance == null || !dateNaissance.trim().matches("^\\d{2}/\\d{2}/\\d{4}$") ||
         email == null || !email.trim().matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$") || tel == null || 
-        !tel.trim().matches("^0\\d{9}$") || adresse == null || adresse.trim().equals("")){
+        !tel.trim().matches("^0\\d{9}$") || adresse == null || adresse.trim().equals("") || disponibilite == null || disponibilite.size() == 0){
             throw new IllegalArgumentException("Les paramètres ne peuvent pas être null ou vides");
         }
 
@@ -27,7 +27,7 @@ public class Secouriste{
         this.email = email;
         this.tel = tel;
         this.adresse = adresse;
-        this.journées = new HashSet<>();
+        this.disponibilite = disponibilite;
     }
     
     //================================
