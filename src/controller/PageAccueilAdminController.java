278da6efe2fafd7d2378f682b67acc635e25e331
Emile <Emile0478@gmail.com>
2025-06-18 21:05:28 +0200

    Avancement sur les controlleurs

--- src/controller/PageAccueilAdminController.java
+++ src/controller/PageAccueilAdminController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class PageAccueilAdminController {
+    public Button deconnexionButton;
+    public Button secouristesButton;
+    public Label idProfilLabel;
+    public Label nomPrenomLabel;
+    public Label mailLabel;
+    public Label phoneLabel;
+    public Button DPSButton;
+    public Label idEvenementLabel;
+    public Label sportLabel;
+    public Label siteLabel;
+    public Label dateLabel;
+    public Label nbSpectateursLabel;
+
+    public void deconnexionPage(ActionEvent actionEvent) {
+    }
+
+    public void secouristePage(ActionEvent actionEvent) {
+    }
+
+    public void dpsPage(ActionEvent actionEvent) {
+    }
 }
