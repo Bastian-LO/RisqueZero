fea8cd6f6037173c55674cc2844e9a55107239ef
Emile <Emile0478@gmail.com>
2025-06-18 18:25:36 +0200

    Changement aux pages pour les controllers

--- src/controller/PageInscriptionController.java
+++ src/controller/PageInscriptionController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class PageInscriptionController {
+
+    @FXML
+    private TextField nomTextField;
+    @FXML
+    private TextField prenomTextField;
+    @FXML
+    private TextField mailTextField;
+    @FXML
+    private TextField dateNaissanceTextField;
+    @FXML
+    private TextField telephoneTextField;
+    @FXML
+    private Button inscriptionButton;
+
+    public void inscriptionHandle(ActionEvent actionEvent) {
+        String nom = nomTextField.getText();
+        String prenom = prenomTextField.getText();
+        String mail = mailTextField.getText();
+        String dateNaissance = dateNaissanceTextField.getText();
+        String telephone = telephoneTextField.getText();
+    }
 }
