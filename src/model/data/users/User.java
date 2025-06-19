2f2f8e5505ae8a363a406f6757e1a7c139bb8dba
Killian <kavril56@gmail.com>
2025-06-19 23:47:54 +0200

    JavaDoc de User

--- src/model/data/users/User.java
+++ src/model/data/users/User.java
@@ -20,7 +20,7 @@ public abstract class User {
     public User(int id, String login, String password, boolean isAdmin) {
         this.id = id;
         this.login = login;
-        this.password = UserMngt.hashPassword(password); // hash password;
+        this.password = UserMngt.hashPassword(password);
         this.isAdmin = isAdmin;
     }
 
@@ -29,27 +29,44 @@ public abstract class User {
      */
     public User(String login, String password, boolean isAdmin) {
         this.login = login;
-        this.password = UserMngt.hashPassword(password); // hash password;
+        this.password = UserMngt.hashPassword(password);
         this.isAdmin = isAdmin;
-        int id = userDAO.create(this); // id is set in create method
+        int id = userDAO.create(this);
     }
 
     //================================
     //           GETTERS
     //================================
 
+    /**
+     * Returns the user's ID
+     * @return the ID of the user
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the user's login
+     * @return the login of the user
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * Returns the user's password
+     * @return the password of the user
+     * @note The password is stored hashed in the database
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Returns true if the user is an administrator, false otherwise
+     * @return whether the user is an administrator or not
+     */
     public boolean isAdmin() {
         return isAdmin;
     }
@@ -58,18 +75,36 @@ public abstract class User {
     //           SETTERS
     //================================
 
+    /**
+     * Sets the user's ID
+     * @param id the new ID
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Sets the user's login
+     * @param login the new login
+     * @throws IllegalArgumentException if the login is null or empty
+     */
     public void setLogin(String login) {
         this.login = login;
     }
 
+    /**
+     * Sets the user's password
+     * @param password the new password to set
+     * @note The password should be stored hashed; ensure it is hashed before setting
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Sets whether the user is an administrator or not
+     * @param admin true if the user is an administrator, false otherwise
+     */
     public void setAdmin(boolean admin) {
         isAdmin = admin;
     }
