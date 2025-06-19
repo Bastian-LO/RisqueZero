865a010505f09e84821894850acbf97e3481e975
Killian <kavril56@gmail.com>
2025-06-19 23:45:11 +0200

    Pull de la JavaDoc service

--- src/model/data/service/UserMngt.java
+++ src/model/data/service/UserMngt.java
@@ -7,10 +7,17 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import model.dao.UserDAO;
+
 public class UserMngt{
 
     private final UserDAO userDAO = new UserDAO();
 
+    /**
+     * Verify the given password for the given login
+     * @param login the login to verify
+     * @param password the password to verify
+     * @return true if the password is valid, false otherwise
+     */
     public boolean verifyPassword(String login, String password) {
         User user = userDAO.findByLogin(login);
         if (user == null){
@@ -20,6 +27,14 @@ public class UserMngt{
         return hashedInput.equals(user.getPassword());
     }
 
+    /**
+     * Hashes a password using SHA-256 algorithm.
+     * Converts the password into a hexadecimal string representation of the hash.
+     *
+     * @param password the plain text password to be hashed
+     * @return the hashed password as a hexadecimal string
+     * @throws RuntimeException if the SHA-256 algorithm is not available
+     */
     public static String hashPassword(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -38,6 +53,11 @@ public class UserMngt{
         }
     }
 
+    /**
+     * Finds a user by its login
+     * @param login the login of the user to search
+     * @return the user object if found, null otherwise
+     */
     public User findUser(String login) {
         return userDAO.findByLogin(login);
     }
